bc54b7b74f223b299f50e6889f6b6da8b09bbaa2
kimkaram <47126446+kimkaram@users.noreply.github.com>
2019-04-26 07:49:59 +0900

    Create MemberIDCheckServlet.java

---
+++ src/Member/MemberIDCheckServlet.java
@@ -0,0 +1,62 @@
+package member.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.model.service.MemberService;
+
+/**
+ * Servlet implementation class MemberIdCheckServlet
+ */
+@WebServlet("/idchk")
+public class MemberIdCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberIdCheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 아이디 중복 체크 검사용 컨트롤러
+		String userId = request.getParameter("userid");
+		
+		int result = new MemberService().selectCheckId(userId);
+		
+		String returnValue = null;
+		if(result == 0)
+			returnValue = "ok";
+		else
+			returnValue = "dup";
+		
+		//서비스 요청한 클라이언트(response)에게 문자열을
+		//전송하려면, 네트워트 출력 스트림(문자기반 스트림)을
+		//생성해야 함
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.append(returnValue);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
